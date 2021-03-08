73af11d9dd29a21789665c92ac14aa5666325664
Mona Lisa <clarkefraser1@gmail.com>
2021-03-08 14:38:25 +1100

    api routes. get. jsonParse

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,20 +1,44 @@
 const router = require("express").Router();
 const noteTakerDB = require("../db/db.json");
+const path = require("path");
+const fs = require("fs");
 // const { v4: uuidv4 } = require('uuid');
 // uuidv4(); // ⇨ '1b9d6bcd-bbfd-4b2d-9b5d-ab8dfbbd4bed'
 
-router.get("/api/notes", (req, res) => {});
+// router.get("/api/notes", (req, res) => {});
 
-router.post("/api/notes", (req, res) => {});
+// router.post("/api/notes", (req, res) => {});
 
-module.exports = router;
 //pseudo below to go above
-const fs = require("fs");
+//module.exports = router => {}
 router.get("/api/notes", function (req, res) {
-   // app.get('/api/notes', (req, res) => res.json(notes));
-  //fs.readfile
-  //JSON.parse
-  //send it to browser res.send or res.json
+  res.json(noteTakerDB);
+  note = JSON.stringify(noteTakerDB);
+  console.log(noteTakerDB);
+  res = JSON.parse(res);
+  console.log(res);
+  fs.readFile(noteTakerDB, function (err, res) {
+    if (error) {
+      return console.log(error);
+    }
+    //console.log(notes);
+    console.log(noteTakerDB);
+
+    //   fs.readFile(__dirname + "/db/db.json", "utf8", function (err, res) {
+    //     if (error) {
+    //       return console.log(error);
+    //     }
+
+    // fs.readFile("db/db.json","utf8", (err, data) => {
+
+    //     if (err) throw err;
+
+    //     var notes = JSON.parse(data);
+    // app.get('/api/notes', (req, res) => res.json(notes));
+    //fs.readfile
+    //JSON.parse
+    //send it to browser res.send or res.json
+  });
 });
 router.post("/api/notes", function (req, res) {
   //req.body notes
@@ -25,3 +49,4 @@ router.post("/api/notes", function (req, res) {
   // if error res.json(false)
   // else res.json(true);
 });
+module.exports = router;
